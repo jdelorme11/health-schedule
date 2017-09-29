42e0fc8fe1c3af29461a992eae7d016f10442598
jdelorme11 <32398951+jdelorme11@users.noreply.github.com>
2017-09-29 16:21:55 -0700

    Create ResourcePatientAllocation.java

---
+++ src/main/java/ca/medgloo/domain/ResourcePatientAllocation.java
@@ -0,0 +1,9 @@
+package ca.medgloo.domain;
+
+public class ResourcePatientAllocation {
+  private int id;
+  private Date date;
+  private int patientId;
+  private int slot;
+  private int alternatePatientId;
+}
