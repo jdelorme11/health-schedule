003aed35563e9539f4280206b717dfe29f905dc9
b0nz0 <b0nz0@b0nz0-Aspire-F5-573T>
2017-09-29 16:24:09 -0700

    Fix formatting

--- src/main/java/ca/medgloo/domain/ResourcePatientAllocation.java
+++ src/main/java/ca/medgloo/domain/ResourcePatientAllocation.java
@@ -3,9 +3,9 @@ package ca.medgloo.domain;
 import java.util.Date;
 
 public class ResourcePatientAllocation {
-  private int id;
-  private Date date;
-  private int patientId;
-  private int slot;
-  private int alternatePatientId;
+	private int id;
+	private Date date;
+	private int patientId;
+	private int slot;
+	private int alternatePatientId;
 }
